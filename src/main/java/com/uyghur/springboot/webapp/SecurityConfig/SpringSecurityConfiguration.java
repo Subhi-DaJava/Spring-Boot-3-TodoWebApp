317803e91976cfe51d0494ef5e520495c7790b44
Subhi YD <subhy809fr@gmail.com>
2022-11-25 10:24:58 +0100

    14e commit: add h2 and spring-data-jpa dependencies, configure url for h2, create SecurityFilterChain for http requests and disable CSRF and frames

--- src/main/java/com/uyghur/springboot/webapp/SecurityConfig/SpringSecurityConfiguration.java
+++ src/main/java/com/uyghur/springboot/webapp/SecurityConfig/SpringSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.uyghur.springboot.webapp.SecurityConfig;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -39,5 +43,30 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	// by default
+	// All URLs are protected
+	// A login form is shown for unauthorized requests
+
+	// configuration SecurityFilterChain: defines a filter chain matched against every request
+	// CSRF disable for enable h2
+	// Frames for h2 
+	
+	@Bean 
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
+		
+		http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated());
+		
+		http.formLogin(withDefaults()); // redirect to login page for unauthorized request
+		
+		http.csrf().disable();
+		
+		// X-Frame-Options enabled => Frames Cannot be used h2-console uses frames => Disable X-Frame-Options header
+		http.headers().frameOptions().disable();
+		
+		return http.build();	
+		
 	}
 }
